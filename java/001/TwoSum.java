728e5ca0a4fcad2f91da6652f6a56c5d6f6e3ef5
liwei <liwei@e-agency-china.com>
2018-10-12 13:39:51 +0800

    leetcode 001

---
+++ java/001/TwoSum.java
@@ -0,0 +1,13 @@
+public class TwoSum {
+    public static void main(String[] args) {
+        TwoSum twoSum = new TwoSum();
+        int[] nums = new int[]{2, 5, 7, 11, 15};
+        int target = 9;
+//        int[] result = twoSum.todo(nums, target);
+//        System.out.println("index1=" + result[0] + "  index2=" + result[1]);
+    }
+
+    public int[] todo(int[] nums, int target) {
+        // TODO
+    }
+}
