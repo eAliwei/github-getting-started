c8450bf96437f6f206deda556d9234241692f2f7
liwei <liwei@e-agency-china.com>
2018-10-17 16:48:25 +0800

    添加冲突文件

--- java/001/TwoSum.java
+++ java/001/TwoSum.java
@@ -4,7 +4,7 @@ public class TwoSum {
 
 	public static void main(String[] args) {
         TwoSum twoSum = new TwoSum();
-        int[] nums = new int[]{2, 5, 7, 11, 15};
+        int[] nums = new int[]{2, 1, 5, 7, 11, 15};
         int target = 9;
         int[] result = twoSum.todo(nums, target);
         System.out.println("index1=" + result[0] + "  index2=" + result[1]);
